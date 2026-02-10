4b8b06ec91b321ed2be674f0e8857fdd48fb8c7b
krasennikolicov@abv.bg <krasennikolicov2@abv.bg>
2026-02-10 11:38:34 +0200

    Cteate Dtos for registration and login. Add controller for registration and login

---
+++ SqlGpt.Dto/RegisterRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SqlGpt.Dto
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; } 
+    }
+}
---
+++ SqlGpt/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SqlGpt.Models;
+
+namespace SqlGpt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+    }
+}
