c4f38f772f0f301c648c1e8ea48a874b47a20d76
krasennikolicov@abv.bg <krasennikolicov2@abv.bg>
2026-02-12 12:55:48 +0200

    register JWT

--- SqlGpt/Program.cs
+++ SqlGpt/Program.cs
@@ -1,8 +1,11 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using SqlGpt.Data;
 using SqlGpt.Models;
+using System.Text;
 
 namespace SqlGpt
 {
@@ -31,6 +34,32 @@ namespace SqlGpt
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            // addvam nastroikite za JWT 
+
+            var jwtSection = builder.Configuration.GetSection("Jwt");
+            var key = jwtSection["Key"];
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+
+                    ValidIssuer = jwtSection["Issuer"],
+                    ValidAudience = jwtSection["Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(key))
+                };
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -42,6 +71,7 @@ namespace SqlGpt
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
